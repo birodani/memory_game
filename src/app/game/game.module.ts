bdc6718ab7b0cb65f11043a7f4e60f7e3a935b33
BÍRÓ DÁNIEL <Daniel.Biro@kh.hu>
2020-01-23 13:29:25 +0100

    9. step add outer translation file to library widget

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,19 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GameComponent } from './game/game.component';
-import { ScoreboardComponent } from './scoreboard/scoreboard.component';
-import { CardComponent } from './card/card.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GameComponent} from './game/game.component';
+import {ScoreboardComponent} from './scoreboard/scoreboard.component';
+import {CardComponent} from './card/card.component';
 import {GameRoutingModule} from './game-routing.module';
-import { NgxListLibModule} from 'ngx-list-lib';
+import {NgxListLibModule, NgxListLibIntl} from 'ngx-list-lib';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material';
 
 
+function NgxListLibScoreBoard() {
+  const intl = new NgxListLibIntl();
+  intl.listTitle = 'ScoreBoard';
+  intl.listTitleHeader = (i) => `${i + 1}. hellyezett`;
+  return intl;
+}
 
 @NgModule({
   declarations: [GameComponent, ScoreboardComponent, CardComponent],
   imports: [
     CommonModule,
     GameRoutingModule,
-    NgxListLibModule
+    NgxListLibModule,
+    FlexLayoutModule,
+    MatCardModule
+  ],
+  providers: [
+    {provide: NgxListLibIntl, useFactory: NgxListLibScoreBoard}
   ]
 })
-export class GameModule { }
+export class GameModule {
+}
