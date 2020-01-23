7ae4bd0d4a5e01344ca8cfcd7f57781b754480c4
BÍRÓ DÁNIEL <Daniel.Biro@kh.hu>
2020-01-23 16:45:04 +0100

    19. step: refactor create card.component

--- src/app/game/card/card.component.ts
+++ src/app/game/card/card.component.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {MemoCard} from '../models/memo-card.model';
+import {MemoGame} from '../memo-game';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  animations: [
+    trigger('stateVisible', [
+      state('true', style({transform: 'rotateY(0)'})),
+      state('false', style({transform: 'rotateY(180deg)'})),
+      transition('true => false', animate('500ms ease')),
+      transition('false => true', animate('500ms ease'))
+    ])
+  ]
 })
 export class CardComponent implements OnInit {
+  @Input()
+  card: MemoCard;
+
+  @Output()
+  cardSelcted = new EventEmitter();
 
   constructor() { }
 
--- src/app/game/game/game.component.ts
+++ src/app/game/game/game.component.ts
@@ -9,15 +9,7 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss'],
-  animations: [
-    trigger('stateVisible', [
-      state('true', style({transform: 'rotateY(0)'})),
-      state('false', style({transform: 'rotateY(180deg)'})),
-      transition('true => false', animate('500ms ease')),
-      transition('false => true', animate('500ms ease'))
-    ])
-  ]
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
   game: MemoGame;
