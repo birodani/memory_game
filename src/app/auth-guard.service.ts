01bc384a45d922a431eb6ab62178bdaaa6a5d15f
BÍRÓ DÁNIEL <Daniel.Biro@kh.hu>
2020-01-23 15:13:19 +0100

    14. step: add auth.guard and guard the url access of the game.component

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {CredentialService} from './credential.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private credentialService: CredentialService) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.credentialService.getUser()) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+}
+
+{
+}
--- src/app/game/game-routing.module.ts
+++ src/app/game/game-routing.module.ts
@@ -2,9 +2,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {GameComponent} from './game/game.component';
 import {ScoreboardComponent} from './scoreboard/scoreboard.component';
+import {AuthGuard} from '../auth-guard.service';
 
 const routes: Routes = [
-  { path: '', component: GameComponent },
+  { path: '', canActivate: [AuthGuard], component: GameComponent },
   { path: 'scoreboard', component: ScoreboardComponent }
 ];
 
