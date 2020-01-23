2e3c3dde89e537f0664ae0659a1867712b5c247b
BÍRÓ DÁNIEL <Daniel.Biro@kh.hu>
2020-01-23 16:01:46 +0100

    17. step: create animation to card transition and style

--- src/app/game/game/game.component.ts
+++ src/app/game/game/game.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {MemoGame} from '../memo-game';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
+  animations: [
+    trigger('stateVisible', [
+      state('true', style({transform: 'rotateY(0)'})),
+      state('false', style({transform: 'rotateY(180deg)'})),
+      transition('true => false', animate('500ms ease')),
+      transition('false => true', animate('500ms ease'))
+    ])
+  ]
 })
 export class GameComponent implements OnInit {
   game: MemoGame;
