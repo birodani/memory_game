31382cd7aa81b0dfa08560240e2c059e202aa879
BÍRÓ DÁNIEL <Daniel.Biro@kh.hu>
2020-01-26 13:21:00 +0100

    20. step: add timer to the game

--- src/app/game/game/game.component.ts
+++ src/app/game/game/game.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MemoGame} from '../memo-game';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 import {ScoreboardService} from '../scoreboard/scoreboard.service';
 import {ScoreboardItem} from '../scoreboard/models/scoreboard-item.model';
 import {CredentialService} from '../../credential.service';
@@ -11,17 +10,42 @@ import {Router} from '@angular/router';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
   game: MemoGame;
+  intervalId = undefined;
+  time = 0;
 
   constructor(private  scoreBoardService: ScoreboardService, private credentialService: CredentialService, private router: Router) {
     this.game = new MemoGame();
   }
 
   ngOnInit() {
+    this.startTimer();
+
     this.game.gameOver
-      .subscribe(x => this.scoreBoardService.saveScoreboard(new ScoreboardItem(this.credentialService.getUser().nickname, new Date().getTime(), 0))
-        .subscribe(() => this.router.navigateByUrl('/game/scoreboard')));
+      .subscribe(x => {
+        this.stopTimer();
+        this.scoreBoardService.saveScoreboard(new ScoreboardItem(this.credentialService.getUser().nickname, new Date().getTime(), this.time))
+          .subscribe(() => this.router.navigateByUrl('/game/scoreboard'));
+      });
   }
+
+  ngOnDestroy(): void {
+    if (this.intervalId !== undefined) {
+      this.stopTimer();
+    }
+  }
+
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      this.time++;
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.intervalId);
+    this.intervalId = undefined;
+  }
+
 
 }
